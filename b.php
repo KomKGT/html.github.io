e3723fde22a5a23fc3c1655c20ea17044cf5571a
KomKGT <kommicro@gmail.com>
2026-03-21 21:39:05 +0700

    fix index.html

--- b.php
+++ b.php
@@ -1,8 +1,8 @@
 <?php
     $s_abc = "abc";
     $s_Hello = "Hello";
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
+    $name = $_GET['name'];
+    $surname = $_GET['surname'];
     echo "Your input Name : ".$name."and Surname : ".$surname;
     echo $s_abc.$s_Hello;
 ?>
